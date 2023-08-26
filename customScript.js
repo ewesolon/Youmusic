440669b3a055f0adda556153486d5d3c8249e0c3
ewesolon <142863195+ewesolon@users.noreply.github.com>
2023-08-26 16:47:00 -0200

    Update customScript.js

--- customScript.js
+++ customScript.js
@@ -1,37 +1,113 @@
-(function() {
-    // Button
-    var button = document.querySelector('.btn-red');
-    if (button) {
-        button.style.backgroundColor = '#cf4242';
-        button.textContent = 'Baixar';
-        button.style.borderRadius = '50px';
-        button.style.border = 'none';
-    }
+(function() {" +
+                    //Button
+                    "   var button = document.querySelector('.btn-red');" +
+                    "   if (button) {" +
+                    "       button.style.backgroundColor = '#cf4242';" +
+                    "       button.textContent = 'Baixar';" +
+                    "       button.style.borderRadius = '50px';" +
+                    "       button.style.border = 'none';" +
+                    "   }" +
+                    //Input
+                    "   var textbox = document.querySelector('#s_input');" +
+                    "   if (textbox) {" +
+                    "       textbox.placeholder = 'Link do YouTube aqui';" +
+                    "       textbox.style.borderRadius = '50px';" +
+                    "       textbox.style.border = 'none';" +
+                    "   }" +
+                    //Title
+                    "   var titleElement = document.querySelector('.title');" +
+                    "   if (titleElement) {" +
+                    "       titleElement.style.fontSize = '30px';" +
+                    "       titleElement.style.color = '#dbdbdd';" +
+                    "       titleElement.textContent = 'YOU MUSIC';" +
+                    "   }" +
 
-    // Input
-    var textbox = document.querySelector('#s_input');
-    if (textbox) {
-        textbox.placeholder = 'Link do YouTube aqui';
-        textbox.style.borderRadius = '50px';
-        textbox.style.border = 'none';
-    }
+                    // Center Section
+                    "   var centerSection = document.querySelector('.ftco-section.center');" +
+                    "   if (centerSection) {" +
+                    "       centerSection.style.height = '80vh';" + // Ajusta a altura para 100% da altura da viewport
+                    "       centerSection.style.display = 'flex';" +
+                    "       centerSection.style.flexDirection = 'column';" + // Alinha itens verticalmente
+                    "       centerSection.style.justifyContent = 'center';" + // Centraliza verticalmente
+                    "   }" +
 
-    // Title
-    var titleElement = document.querySelector('.title');
-    if (titleElement) {
-        titleElement.style.fontSize = '30px';
-        titleElement.style.color = '#dbdbdd';
-        titleElement.textContent = 'YOU MUSIC';
-    }
 
-    // Center Section
-    var centerSection = document.querySelector('.ftco-section.center');
-    if (centerSection) {
-        centerSection.style.height = '80vh';
-        centerSection.style.display = 'flex';
-        centerSection.style.flexDirection = 'column';
-        centerSection.style.justifyContent = 'center';
-    }
+                    "   var containerMain = document.querySelector('.container-main');" +
+                    "   if (containerMain) {" +
+                    "       containerMain.style.background = '#191a1f';" +
+                    "   }" +
 
-    // Rest of your customizations...
-})();
+                    "   var sectionSearch = document.querySelector('.section-search');" +
+                    "   if (sectionSearch) {" +
+                    "       sectionSearch.style.backgroundColor = '#191a1f';" +
+                    "   }" +
+
+
+
+
+                    //Remover elementos
+                    "   var alertElement = document.querySelector('.alert.alert-success.alert-gr');" +
+                    "   if (alertElement) {" +
+                    "       alertElement.parentNode.removeChild(alertElement);" +
+                    "   }" +
+
+                    "   var titleHeader = document.querySelector('.title');" +
+                    "   if (titleHeader && titleHeader.textContent.includes('X2Download - Melhor Downloader de MP3 do Youtube 2023')) {" +
+                    "       titleHeader.parentNode.removeChild(titleHeader);" +
+                    "   }" +
+
+                    "   var paragraphElement = document.querySelector('.mw70');" +
+                    "   if (paragraphElement && paragraphElement.textContent.includes('Youtube to Mp3')) {" +
+                    "       paragraphElement.parentNode.removeChild(paragraphElement);" +
+                    "   }" +
+
+                    "   var listItemsMoreElement = document.querySelector('.listitem.items-more');" +
+                    "   if (listItemsMoreElement) {" +
+                    "       listItemsMoreElement.parentNode.removeChild(listItemsMoreElement);" +
+                    "   }" +
+
+
+                    "   var listIconElement = document.querySelector('.listicon.liststep');" +
+                    "   if (listIconElement) {" +
+                    "       listIconElement.parentNode.removeChild(listIconElement);" +
+                    "   }" +
+
+                    "   var sectionLeftElements = document.querySelectorAll('.ftco-section.section-left');" +
+                    "   for (var i = 0; i < sectionLeftElements.length; i++) {" +
+                    "       sectionLeftElements[i].parentNode.removeChild(sectionLeftElements[i]);" +
+                    "   }" +
+
+                    "   var titleToRemove = document.querySelector('.title');" +
+                    "   if (titleToRemove && titleToRemove.textContent === 'Como baixar mp3 do Youtube mais rápido') {" +
+                    "       titleToRemove.parentNode.removeChild(titleToRemove);" +
+                    "   }" +
+
+                    "   var adsElement = document.querySelector('#ads_b');" +
+                    "   if (adsElement) {" +
+                    "       adsElement.parentNode.removeChild(adsElement);" +
+                    "   }" +
+
+                    "   var titleElementToRemove = document.querySelector('h2.title');" +
+                    "   if (titleElementToRemove) {" +
+                    "       titleElementToRemove.parentNode.removeChild(titleElementToRemove);" +
+                    "   }" +
+
+                    "   var howToDownloadElement = document.querySelector('h2.title');" +
+                    "   if (howToDownloadElement) {" +
+                    "       howToDownloadElement.parentNode.removeChild(howToDownloadElement);" +
+                    "   }" +
+
+                    "   var topnav = document.querySelector('.topnav');" +
+                    "   if (topnav) {" +
+                    "   topnav.remove();" +
+                    "   }" +
+
+                    "   var paragraph = document.querySelector('p');" +
+                    "   if (paragraph) {" +
+                    "       paragraph.parentNode.removeChild(paragraph);" +
+                    "   }" +
+
+
+                    // Rest of your customizations
+                    // ...
+                    "})()";
